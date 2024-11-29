4976d73bdc0b81b3756843948e8abbd38798aeff
ethicsadvise <ethicsadvisorproject@gmail.com>
2024-11-29 20:40:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def save_user_data(user_id, data):
         json.dump(data, file)
 
 def main():
-    endpoint_url = "https://c0da-34-16-134-159.ngrok-free.app/predict"  # Endpoint URL from .env
+    endpoint_url = "https://5d43-34-16-177-174.ngrok-free.app/predict"  # Endpoint URL from .env
 
     user_id = get_user_id()
     user_data = load_user_data(user_id)
